4af53a1ed3914cecca0907216156a401bff3201b
Snehashis Lenka <39059004+SnehashisWalk@users.noreply.github.com>
2022-01-12 22:26:48 +0530

    update v0.3

--- frontend/src/components/posts/Post.js
+++ frontend/src/components/posts/Post.js
@@ -5,17 +5,24 @@ export default function Post(props) {
   console.log(props);
   return (
     <div className="post-container">
-      <div className="post-left"></div>
+      <div className="post-left">
+        {props.content.contentImg ? (
+          <img src={props.content.contentImg} alt="content" />
+        ) : (
+          ""
+        )}
+      </div>
       <div className="post-right">
         <div className="post-header">
-          <h2 className="post-heading">Demo Heading</h2>
+          <h2 className="post-heading">
+            {props.content.title ? props.content.title : "Demo Heading"}
+          </h2>
         </div>
         <div className="post-content">
           <p className="post">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam urna
-            ex, vehicula eu rutrum eu, luctus posuere turpis. Nam facilisis
-            tellus nec erat viverra semper. Quisque scelerisque tempus nunc ut
-            pharetra. Vivamus et finibus turpis.
+            {props.content.shortDesc
+              ? props.content.shortDesc
+              : "short description"}
           </p>
         </div>
       </div>
